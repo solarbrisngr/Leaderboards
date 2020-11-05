721c9348887484293f40749fe92a499479e32cdb
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-11-05 15:38:56 -0700

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rig;
+    private float startTime;
+    private float timeTaken;
+    private int collectablesPicked;
+    public int maxCollectables = 10;
+    private bool isPlaying;
+
+    public GameObject playButton;
+    public TextMeshProUGUI curTimeText;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPlaying)
+            return;
+
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+        curTimeText.text = (Time.time - startTime).ToString("F2");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            collectablesPicked++;
+            Destroy(other.gameObject);
+            if (collectablesPicked == maxCollectables)
+                End();
+        }
+    }
+
+    public void Begin()
+    {
+        playButton.SetActive(false);
+        startTime = Time.time;
+        isPlaying = true;
+    }
+
+    void End()
+    {
+        playButton.SetActive(true);
+        timeTaken = Time.time - startTime;
+        isPlaying = false;
+        Leaderboard.instance.SetLeaderboardEntry(-Mathf.RoundToInt(timeTaken * 1000.0f));
+    }
+
+}
